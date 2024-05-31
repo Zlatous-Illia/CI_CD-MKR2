6253c9d6ffdff1fdc7dec9738a8eac7ab4cc520d
Zlatous-Illia <zlatous_illia@knu.ua>
2024-05-31 14:30:24 +0300

    Changing html templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from recipes.models import Book, Author
+from .models import Recipe
 
 
 def main(request):
-    # Отримати всі рецепти за 2023 рік
     recipes = Recipe.objects.filter(year=2023)
-    return render(request, 'main.html', {'recipes': recipes})
+    return render(request, 'recipes/main.html', {'recipes': recipes})
 
 def recipe_detail(request, recipe_id):
-    # Отримати деталі певного рецепту за його id
     recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, 'recipe_detail.html', {'recipe': recipe})
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
