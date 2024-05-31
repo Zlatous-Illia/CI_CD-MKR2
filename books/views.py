62b156bb3d760673a49c226dbb4ec360c3f81b30
Zlatous-Illia <zlatous_illia@knu.ua>
2024-05-31 14:20:26 +0300

    Changing views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from books.models import Book, Author
 
-# Create your views here.
+
+def main(request):
+    # Отримати всі рецепти за 2023 рік
+    recipes = Recipe.objects.filter(year=2023)
+    return render(request, 'main.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    # Отримати деталі певного рецепту за його id
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
